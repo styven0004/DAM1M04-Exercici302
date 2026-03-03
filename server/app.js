b6b6fa8c4b0c62c2e1f88e53817a69d2f3fe6aa1
styven0004 <styvencatagua@gmail.com>
2026-03-03 15:45:50 +0100

    Canvi app.js

--- server/app.js
+++ server/app.js
@@ -14,10 +14,10 @@ const isProxmox = !!process.env.PM2_HOME;
 const db = new MySQL();
 if (!isProxmox) {
   db.init({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: 'root',
+    user: 'appuser',
+    password: '1234',
     database: 'sakila'
   });
 } else {
